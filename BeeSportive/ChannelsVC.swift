8cbd97788778c9d1fc83d771cb499ecf833877f1
Efe Helvaci <efe.459@gmail.com>
2016-10-15 00:07:22 +0300

    Code readability

--- BeeSportive/ChannelsVC.swift
+++ BeeSportive/ChannelsVC.swift
@@ -55,46 +55,71 @@ class ChannelsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
         let tabBarCont = tabBarController as! TabBarController
         tabBarCont.menuButton.isHidden = true
         tabBarCont.tabBar.isHidden = true
+        
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
+        
         super.viewWillDisappear(animated)
         let tabBarCont = tabBarController as! TabBarController
         tabBarCont.menuButton.isHidden = false
         tabBarCont.tabBar.isHidden = false
+        
     }
+    
+    // MARK: - Table View Delegate Methods
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if isSearching { return filteredChannels.count }
-        else { return channels.count }
+        
+        if isSearching {
+            return filteredChannels.count
+        } else {
+            return channels.count
+        }
+        
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         if let cell = tableView.dequeueReusableCell(withIdentifier: "ChannelCell") as? ChannelCell {
-            if isSearching { cell.configureCell(filteredChannels[(indexPath as NSIndexPath).row]) }
-            else { cell.configureCell(channels[(indexPath as NSIndexPath).row]) }
+            if isSearching {
+                cell.configureCell(filteredChannels[(indexPath as NSIndexPath).row])
+            } else {
+                cell.configureCell(channels[(indexPath as NSIndexPath).row])
+            }
             return cell
-        } else { return UITableViewCell() }
+        } else {
+            return UITableViewCell()
+        }
+        
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if isSearching { self.selectedChannelID = filteredChannels[(indexPath as NSIndexPath).row].id }
-        else { self.selectedChannelID = channels[(indexPath as NSIndexPath).row].id }
+        
+        if isSearching {
+            self.selectedChannelID = filteredChannels[(indexPath as NSIndexPath).row].id
+        } else {
+            self.selectedChannelID = channels[(indexPath as NSIndexPath).row].id
+        }
+        
         guard let cell = tableView.cellForRow(at: indexPath) as? ChannelCell else { return }
         performSegue(withIdentifier: "toChatSegue", sender: cell)
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         if segue.identifier == "toChatSegue" {
             if let destVC = segue.destination as? ChatVC {
                 destVC.channelID = self.selectedChannelID
@@ -102,20 +127,26 @@ class ChannelsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
                 destVC.navigationItem.title = cell.title.text
             }
         }
+        
     }
-
+    
+    // MARK: - Search Bar Delegate Methods
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         self.view.endEditing(true)
     }
-
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
         if searchBar.text == nil || searchBar.text == "" {
             isSearching = false
         } else {
             isSearching = true
             let key = searchBar.text!.capitalized
             filteredChannels = channels.filter({$0.title.range(of: key) != nil})
-        }; tableView.reloadData()
+        }
+        tableView.reloadData()
+        
     }
-
+    
 }
--- BeeSportive/ChatVC.swift
+++ BeeSportive/ChatVC.swift
@@ -11,18 +11,18 @@ import Firebase
 import JSQMessagesViewController
 
 class ChatVC: JSQMessagesViewController {
-
+    
     var channelID: String!
     var messages = [JSQMessage]()
-
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        
         automaticallyScrollsToMostRecentMessage = true
-        
         self.senderId = FIRAuth.auth()?.currentUser?.uid
         self.senderDisplayName = FIRAuth.auth()?.currentUser?.displayName
-
+        
+        // Getting messages and users one by one
         REF_CHANNELS.child(channelID).child("messages").observe(.childAdded, with: { snapshot in
             guard let data = snapshot.value as? Dictionary<String, String> else { return }
             let senderId = data["senderId"]!
@@ -31,49 +31,59 @@ class ChatVC: JSQMessagesViewController {
                 self.addMessage(data["message"]!, senderId: data["senderId"]!, senderDisplayName: displayName, date: Date())
             })
         })
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
         let tabBarCont = tabBarController as! TabBarController
         tabBarCont.menuButton.isHidden = true
         tabBarCont.tabBar.isHidden = true
+        
     }
-
+    
+    // MARK: - Collection View Delegate Methods:
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return messages.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
         
+        let cell = super.collectionView(collectionView, cellForItemAt: indexPath) as! JSQMessagesCollectionViewCell
         let message = messages[indexPath.item]
         if message.senderId == senderId {
             cell.textView!.textColor = UIColor.white
         } else {
             cell.textView!.textColor = UIColor.black
         }
-        
         return cell
     }
-
+    
+    // MARK: - JSQMessages Methods
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
         return messages[indexPath.row]
     }
-
+    
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+        
         let factory = JSQMessagesBubbleImageFactory()
         let outgoingBubbleImage = factory?.outgoingMessagesBubbleImage(
             with: UIColor.jsq_messageBubbleBlue())
         let incomingBubbleImage = factory?.incomingMessagesBubbleImage(
             with: UIColor.jsq_messageBubbleLightGray())
         let message = messages[indexPath.item]
-        if message.senderId == senderId { return outgoingBubbleImage }
-        else { return incomingBubbleImage }
+        
+        if message.senderId == senderId {
+            return outgoingBubbleImage
+        } else {
+            return incomingBubbleImage
+        }
+        
     }
-
+    
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        
         let name = messages[indexPath.row].senderDisplayName
         let components = name?.components(separatedBy: " ")
         var initials = ""
@@ -82,17 +92,22 @@ class ChatVC: JSQMessagesViewController {
         }
         let factory = JSQMessagesAvatarImageFactory.self
         return factory.avatarImage(withUserInitials: initials, backgroundColor: UIColor.gray, textColor: UIColor.white, font: UIFont.systemFont(ofSize: 12), diameter: 30)
+        
     }
-
+    
     override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        
         let dict: Dictionary<String, String> = ["message":text, "senderId":senderId, "date":Date().description]
         REF_CHANNELS.child(channelID).child("messages").childByAutoId().setValue(dict)
         REF_CHANNELS.child(channelID).child("lastMessage").setValue(dict)
         finishSendingMessage(animated: true)
+        
     }
-
+    
     override func didPressAccessoryButton(_ sender: UIButton) {
+        
         self.inputToolbar.contentView!.textView!.resignFirstResponder()
+        
         let sheet = UIAlertController(title: "Media messages", message: nil, preferredStyle: .actionSheet)
         let photoAction = UIAlertAction(title: "Send photo", style: .default) { (action) in
             let photoItem = JSQPhotoMediaItem(image: UIImage(named: "pp"))
@@ -103,44 +118,53 @@ class ChatVC: JSQMessagesViewController {
             self.addMedia(locationItem)
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
         sheet.addAction(photoAction)
         sheet.addAction(locationAction)
         sheet.addAction(cancelAction)
         self.present(sheet, animated: true, completion: nil)
+        
     }
-
+    
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, didTapAvatarImageView avatarImageView: UIImageView!, at indexPath: IndexPath!) {
-
+        
     }
-
+    
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
         return 20
     }
-
+    
     override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
         return NSAttributedString(string: messages[indexPath.row].senderDisplayName)
     }
-
+    
+    // MARK: - JSQMessages Data Creation Methods
     func buildLocationItem() -> JSQLocationMediaItem {
+        
         let ferryBuildingInSF = CLLocation(latitude: 37.795313, longitude: -122.393757)
         let locationItem = JSQLocationMediaItem()
         locationItem.setLocation(ferryBuildingInSF) {
             self.collectionView!.reloadData()
         }
         return locationItem
+        
     }
-
+    
     func addMedia(_ media:JSQMediaItem) {
+        
         let message = JSQMessage(senderId: senderId, displayName: senderDisplayName, media: media)
         self.messages.append(message!)
         self.finishSendingMessage(animated: true)
+        
     }
-
+    
     func addMessage(_ text: String, senderId: String, senderDisplayName: String, date: Date) {
+        
         let message = JSQMessage(senderId: senderId, senderDisplayName: senderDisplayName, date: date, text: text)
         messages.append(message!)
         self.collectionView.reloadData()
         finishReceivingMessage(animated: true)
+        
     }
     
 }
